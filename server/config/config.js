57a06e280b337542fe2e1bd03daac9fbf02b8c30
Jpicazo <juliopicazo06@gmail.com>
2019-01-29 09:45:53 -0600

    version con rutas

--- server/config/config.js
+++ server/config/config.js
@@ -7,10 +7,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// }else{
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+}
 
 urlDB = 'mongodb://fizharts:_gohan182@ds121674.mlab.com:21674/cafedos';
 
